dd99734438ccd449db168d7d7b1716235f792bd1
iWw <i.1214@yahoo.com>
2021-11-16 13:27:28 +0800

    update pkgConfig

--- Package.swift
+++ Package.swift
@@ -9,6 +9,6 @@ let package = Package(
         .library(name: "cocilib", targets: ["cocilib"])
     ],
     targets: [
-        .systemLibrary(name: "cocilib")
+        .systemLibrary(name: "cocilib", pkgConfig: "ocilib")
     ]
 )
