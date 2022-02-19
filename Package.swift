718c53cda017093cd7bb2c0b98a20fc38304444f
iWw <i.1214@yahoo.com>
2022-02-19 20:47:21 +0800

    update

--- Package.swift
+++ Package.swift
@@ -5,6 +5,9 @@ import PackageDescription
 
 let package = Package(
     name: "cocilib",
+    platforms: [
+        .macOS(.v11)
+    ],
     products: [
         .library(name: "cocilib", targets: ["cocilib"])
     ],
